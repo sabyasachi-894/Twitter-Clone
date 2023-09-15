aa3e5a956fef1e04dd18ca2e513859f746ea49c5
sabyasachi-894 <sabyasachipapan21@gmail.com>
2023-09-15 20:00:35 +0530

    First Commit

---
+++ client/src/components/EditTweet/EditTweet.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { Link, useLocation, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { changeProfile, logout } from "../../redux/userSlice";
+import { useNavigate } from "react-router-dom";
+
+
+
+
+const EditTweet = ({ tweet, setOpen }) => {
+    
+
+    const { currentUser } = useSelector((state) => state.user);
+    const [editText, setEditText] = useState("");
+
+    const handleEdit = async (e) => {
+        e.preventDefault();
+    
+        try {
+          console.log(`${tweet._id}`);
+          const editedTweet = await axios.put(`/tweets/edit/${tweet._id}`, {
+            id: currentUser._id,
+            description: editText,
+          });
+          
+        }catch (err) {
+          console.log("error", err);
+        }
+      };
+
+return (
+    <div className="absolute w-full h-full top-0 left-0 bg-transparent flex items-center justify-center">
+      <div className="w-[600px] h-[600px] bg-slate-200 rounded-lg p-8 flex flex-col gap-4 relative">
+        <button
+          onClick={() => setOpen(false)}
+          className="absolute top-3 right-3 cursor-pointer"
+        >
+          X
+        </button>
+        <h2 className="font-bold text-xl">Edit Tweet</h2>
+        <textarea
+          type="text"
+          placeholder="Enter the updates"
+          onChange={(e) => setEditText(e.target.value)}
+          maxLength={280}
+          className="bg-slate-200 rounded-lg w-full p-2"
+        ></textarea>
+        <button
+        onClick={handleEdit}
+          className="bg-blue-500 text-white py-2 rounded-full"
+        >
+          Update
+        </button>
+      </div>
+    </div>
+  );
+};
+
+
+export default EditTweet;
---
+++ client/src/components/Tweet/Tweet.jsx
@@ -0,0 +1,142 @@
+import axios from "axios";
+import React, { useState } from "react";
+import formatDistance from "date-fns/formatDistance";
+
+import { useEffect } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+import EditTweet from "../EditTweet/EditTweet";
+
+const Tweet = ({ tweet, setData }) => {
+  const [open, setOpen] = useState(false);
+  const { currentUser } = useSelector((state) => state.user);
+
+  const [userData, setUserData] = useState();
+
+  const dateStr = formatDistance(new Date(tweet.createdAt), new Date());
+  const location = useLocation().pathname;
+  const { id } = useParams();
+
+  console.log(location);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const findUser = await axios.get(`/users/find/${tweet.userId}`);
+
+        setUserData(findUser.data);
+      } catch (err) {
+        console.log("error", err);
+      }
+    };
+
+    fetchData();
+  }, [tweet.userId, tweet.likes]);
+
+  const handleLike = async (e) => {
+    e.preventDefault();
+
+    try {
+      const like = await axios.put(`/tweets/${tweet._id}/like`, {
+        id: currentUser._id,
+      });
+
+      if (location.includes("profile")) {
+        const newData = await axios.get(`/tweets/user/all/${id}`);
+        setData(newData.data);
+      } else if (location.includes("explore")) {
+        const newData = await axios.get(`/tweets/explore`);
+        setData(newData.data);
+      } else {
+        const newData = await axios.get(`/tweets/timeline/${currentUser._id}`);
+        setData(newData.data);
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+   
+   
+
+
+
+
+  const handleDelete = async (e) => {
+    e.preventDefault();
+
+    try {
+      console.log(`${tweet._id}`);
+      await axios.delete(`/tweets/${tweet._id}`, {
+        id: currentUser._id,
+      });
+      
+    }catch (err) {
+      console.log("error", err);
+    }
+  };
+
+
+
+  const handleEdit = async (e) => {
+    e.preventDefault();
+
+    try {
+      console.log(`${tweet._id}`);
+      // await axios.delete(`/tweets/${tweet._id}`, {
+      //   id: currentUser._id,
+      // });
+      
+    }catch (err) {
+      console.log("error", err);
+    }
+  };
+
+
+
+
+
+
+  return (
+    <>
+    <div>
+      {userData && (
+        <>
+          <div className="flex space-x-2">
+            {/* <img src="" alt="" /> */}
+            <Link to={`/profile/${userData._id}`}>
+              <h3 className="font-bold">{userData.username}</h3>
+            </Link>
+
+            <span className="font-normal">@{userData.username}</span>
+            <p> - {dateStr}</p>
+          </div>
+
+          <p>{tweet.description}</p>
+          <button onClick={handleLike}>
+            {tweet.likes.includes(currentUser._id) ? (
+              <FavoriteIcon className="mr-2 my-2 cursor-pointer"></FavoriteIcon>
+            ) : (
+              <FavoriteBorderIcon className="mr-2 my-2 cursor-pointer"></FavoriteBorderIcon>
+            )}
+            {tweet.likes.length}
+          </button>
+          <button onClick={handleDelete}>
+          <DeleteIcon className="mr-2 my-2 cursor-pointer"></DeleteIcon>
+          </button>
+          <button onClick={() => setOpen(true)}>
+          <EditIcon className="mr-2 my-2 cursor-pointer"></EditIcon>
+          </button>
+        </>
+      )}
+    </div>
+    {open && <EditTweet tweet={tweet} setOpen={setOpen} />}
+    </>
+  );
+};
+
+export default Tweet;
